c6ae8e0b8693be63b0e20d07017dd16ac90d8ff3
AlexHam1702 <55348780+AlexHam1702@users.noreply.github.com>
2022-04-01 11:05:45 +0200

    mise en place du module neopixel

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
 let Display = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
+let strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
 basic.forever(function () {
     if (grove.measureInCentimeters(DigitalPin.P0) == 15) {
         Display.show(0)
         basic.pause(100)
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     } else {
         Display.show(grove.measureInCentimeters(DigitalPin.P0))
         basic.pause(100)
         for (let index = 0; index < 4; index++) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
             basic.showLeds(`
                 # # # # #
                 # . . . #
@@ -15,6 +25,7 @@ basic.forever(function () {
                 # # # # #
                 `)
             basic.pause(300)
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
             basic.showLeds(`
                 . . . . .
                 . . . . .
