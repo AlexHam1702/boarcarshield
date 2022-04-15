c0b7160145091370e079a85dde24a122c627bc00
AlexHam1702 <55348780+AlexHam1702@users.noreply.github.com>
2022-04-15 11:04:43 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 let strip: neopixel.Strip = null
 let Display = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
-let range = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+let range = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
 basic.forever(function () {
-    if (grove.measureInCentimeters(DigitalPin.P0) == 25) {
+	
+})
+loops.everyInterval(200, function () {
+    if (grove.measureInCentimeters(DigitalPin.P2) >= 25) {
         Display.show(0)
         basic.pause(100)
         range.showColor(neopixel.colors(NeoPixelColors.Black))
@@ -16,10 +19,10 @@ basic.forever(function () {
     } else {
         Display.show(grove.measureInCentimeters(DigitalPin.P0))
         basic.pause(100)
-        strip = range.range(grove.measureInCentimeters(DigitalPin.P0) - -1, 3)
+        strip = range.range(grove.measureInCentimeters(DigitalPin.P0) / 3, -1)
         for (let index = 0; index < 4; index++) {
             basic.showString("ATTENTION")
-            strip.setPixelColor(255, neopixel.colors(NeoPixelColors.Red))
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
         }
         range.showColor(neopixel.colors(NeoPixelColors.Black))
     }
